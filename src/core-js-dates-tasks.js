b0aa204a23617db1e29030d8448528665f006e01
ki8vi <oneofbln@gmail.com>
2024-03-19 12:44:59 +0600

    feat: solve getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -56,8 +56,17 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const days = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wendesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  };
+  return days[new Date(date).getUTCDay()];
 }
 
 /**
